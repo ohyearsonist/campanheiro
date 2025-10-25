4cd6f01587009b3653a49d62768cbff61d3f29d1
ohyearsonist <ohyearsonist@gmail.com>
2025-10-25 16:06:52 -0300

    Transfer data to another page

--- main.js
+++ main.js
@@ -2,21 +2,21 @@ let fileInput = document.getElementById("sheetUpload");
 let baseFileButton = document.getElementById("baseFile");
 
 baseFileButton.onclick = () => {
-    var link = document.createElement("a");
-    link.download = "nome do seu personagem.json";
-    link.href = "./baseSheet.json";
-    link.click();
-}
-
-function loadSheet(sheetFile) {
-   sheetData = JSON.parse(sheetFile);
-
-   console.log(sheetData);
-}
+  var link = document.createElement("a");
+  link.download = "nome do seu personagem.json";
+  link.href = "./baseSheet.json";
+  link.click();
+};
 
 fileInput.onchange = () => {
   const reader = new FileReader();
-  reader.onload = (e) => loadSheet(e.target.result);
+  reader.onload = (e) => {
+    sheetData = e.target.result;
+
+    var link = document.createElement("a");
+    link.href = "./sheet.html?sheetData=" + encodeURI(sheetData);
+    link.click();
+  };
 
   for (let file of fileInput.files) {
     reader.readAsText(file);
